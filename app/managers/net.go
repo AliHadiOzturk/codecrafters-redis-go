72ab091f2da05487f186176a3581687ec83422bc
Ali Hadi Öztürk <ozturkalihadi@gmail.com>
2024-02-21 17:21:28 +0300

    ping message implemented

---
+++ app/managers/net.go
@@ -0,0 +1,54 @@
+package managers
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+type NetManager struct {
+	listener *net.Listener
+
+	message chan []byte
+}
+
+func NewNetManager() *NetManager {
+	return &NetManager{
+		message: make(chan []byte),
+	}
+}
+
+func (n *NetManager) Init() {
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
+	n.listener = &l
+
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+}
+
+func (n *NetManager) Start() {
+	for {
+		connection, err := (*n.listener).Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+		}
+
+		data := make([]byte, 1024)
+
+		connection.Read(data)
+
+		fmt.Println("Received data: ", string(data))
+
+		_, err = connection.Write([]byte("+PONG\r\n"))
+
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		connection.Close()
+	}
+}
--- app/server.go
+++ app/server.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	// Uncomment this block to pass the first stage
-	"net"
-	"os"
+	"github.com/codecrafters-io/redis-starter-go/app/managers"
 )
 
 func main() {
@@ -12,15 +9,9 @@ func main() {
 	// fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+
+	netManager := managers.NewNetManager()
+	netManager.Init()
+	netManager.Start()
+
 }
