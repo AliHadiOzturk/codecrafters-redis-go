40ef2a20150a554464e4762e91ecde02edbcbf88
Ali Hadi Öztürk <ozturkalihadi@gmail.com>
2024-03-11 22:09:25 +0300

    set expire option added

--- commands/set.go
+++ commands/set.go
@@ -1,6 +1,51 @@
 package commands
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
 var Registry map[string]interface{}
+
+var options = map[string]interface{}{
+	"PX": expire,
+}
+
+func keyInArray(key string, arr []string) (bool, int) {
+	for i, item := range arr {
+		if item == key || key == strings.ToUpper(item) {
+			return true, i
+		}
+	}
+	return false, -1
+}
+
+func handleOptions(parameters []string) error {
+	if len(parameters) < 3 {
+		return errors.New("Not enough parameters")
+	}
+
+	for key, value := range options {
+		found, index := keyInArray(key, parameters)
+		if found {
+
+			optionValue := parameters[index+1]
+			if optionValue == "" {
+				return errors.New("Invalid value for option")
+			}
+
+			err := value.(func(string, string) error)(parameters[0], optionValue)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
 
 func Set(parameters []string) (string, error) {
 	key := parameters[0]
@@ -10,7 +55,29 @@ func Set(parameters []string) (string, error) {
 		Registry = make(map[string]interface{})
 	}
 
+	err := handleOptions(parameters)
+
+	if err != nil {
+		return err.Error(), err
+	}
+
 	Registry[key] = value
 
 	return "+OK", nil
+}
+
+func expire(key string, value string) error {
+
+	ms, err := strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+
+	go (func() {
+		time.Sleep(time.Duration(ms) * time.Millisecond)
+		fmt.Println("Deleting key: ", key)
+		delete(Registry, key)
+	})()
+
+	return nil
 }
